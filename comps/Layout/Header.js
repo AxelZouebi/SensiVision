fe80bad2ed9c4af0cc195db0ea6dc41ed37e1246
Axel <72017288+AxelZouebi@users.noreply.github.com>
2021-06-28 16:32:24 +0200

    Responsive design for mobile : configuration added in header

--- comps/Layout/Header.js
+++ comps/Layout/Header.js
@@ -5,6 +5,7 @@ const Header = () => {
         <Head>
             <title>Sensivision</title>
             <meta name="description" content="Festival pour le soutien de la cause animal" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link rel="icon" href="/Logo.png" />
             <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         </Head>
